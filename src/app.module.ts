c0f941e8e923868a6380d98dc882b6296b12163b
Danylo Korsun <dakorsun@gmail.com>
2024-03-18 17:35:17 +0200

    build(typeorm): added typeorm with pg connection (#7)
    * build(typeorm): added typeorm with pg connection
    
    chore(docker): tweaked few properties for pg
    chore(env): updated .example.env with DATABASE_URL
    chore(dependencies): pnpm version updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfModule } from './typeorm/typeorm.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmConfModule,
   ],
   controllers: [AppController],
   providers: [AppService],
